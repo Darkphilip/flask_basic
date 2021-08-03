bc882ac04616b589fd2f65841e5d8d313ef2378d
DarkPhilip <yoonlucky0@gmail.com>
2021-08-03 16:32:06 +0900

    blog login commit

---
+++ 08_flask_ABTest_Login/blog_view/blog.py
@@ -0,0 +1,40 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from flask import Flask, Blueprint, json, request,render_template, make_response, jsonify, redirect, url_for
+from flask_login import UserMixin, login_user, current_user, logout_user
+from blog_control.user_mgmt import User
+import datetime
+
+
+blog_abtest = Blueprint('blog', __name__)
+
+@blog_abtest.route('/set_email', methods = ['GET', 'POST'])
+def set_email():
+  if request.method == 'GET':
+    print('set_email:', request.args.get('user_email'))
+    return redirect(url_for('blog.test_blog'))
+  else:
+    # print('set_email:', request.headers)
+    # content type이 application/json인 경우
+    # print('set_email:', request.get_json())
+    print('set_email:', request.form['user_email'])
+    user = User.create(request.form['user_email'], 'A')
+    login_user(user, remember=True, duration=datetime.timedelta(days=365))
+    return redirect(url_for('blog.test_blog'))
+
+  # return redirect('/blog/test_blog')
+  # return make_response(jsonify(success = True), 200)
+
+@blog_abtest.route('/test_blog')
+def test_blog():
+  if current_user.is_authenticated:
+    return render_template('blog_A.html', user_email=current_user.user_email)
+  else:
+    return render_template('blog_A.html')
+
+@blog_abtest.route('/logout')
+def logout():
+  User.delete(current_user.id)
+  logout_user()
+  return redirect(url_for('blog.test_blog'))
